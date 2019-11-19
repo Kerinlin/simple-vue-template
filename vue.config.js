a3e193f6312667c97dae9cb89ee8be19c1fb9d3e
kerinlin <18296613547@qq.com>
2019-11-19 17:48:20 +0800

    Fix loader

--- vue.config.js
+++ vue.config.js
@@ -4,19 +4,6 @@ module.exports = {
   chainWebpack: config => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
     types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)));
-    config
-      .module
-      .rule('vue')
-      .test(/\.vue$/)
-      .use('iview-loader')
-      .loader('iview-loader')
-      .tap(options => {
-        // 修改它的选项...
-        return {
-          prefix: false
-        };
-      })
-      .end();
   },
   css: {
     loaderOptions: { // 向 CSS 相关的 loader 传递选项
