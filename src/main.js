33b10163b501ea0a04af06b96c0744a7449264af
kerinlin <18296613547@qq.com>
2019-11-19 17:53:18 +0800

    Fix route bug

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/index.js';
-import './common/css/index.css'
+import './common/css/index.css';
 
 Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  i18n,
   render: h => h(App),
 }).$mount('#app');
